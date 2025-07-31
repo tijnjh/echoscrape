8be3320468c290add3031a5dabb95e08f8cb6a46
tijn <tjonash@icloud.com>
2025-07-31 13:42:48 +0900

    fix: oopsie

--- Sources/echoscrape/Utilities/logger.swift
+++ Sources/echoscrape/Utilities/logger.swift
@@ -2,10 +2,10 @@ import Rainbow
 
 public class Logger {
     static func success(_ message: String) {
-        print("\("✖".red) \(message)")
+        print("\("✔".green) \(message)")
     }
     static func fail(_ message: String) {
-        print("\("✔".green) \(message)")
+        print("\("✖".red) \(message)")
     }
     static func info(_ message: String) {
         print("\("ℹ".blue) \(message)")
